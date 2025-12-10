920e482c2a1e190571fca62d36b0701a2cfbf25a
Xollll <mzlkrnainn@gmail.com>
2025-12-11 00:59:04 +0800

    clean up the project

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import '../theme/app_theme.dart';
 import '../widgets/enhanced_app_bar.dart';
 import '../providers/app_settings_provider.dart';
 
